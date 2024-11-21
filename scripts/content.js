4dd825725477960f832736a2074980faf59aa261
henrik2123 <henrik.pedersen@intility.no>
2024-11-21 13:45:09 +0100

    Videre setup yappadap

---
+++ scripts/content.js
@@ -0,0 +1,8 @@
+async function translatePage() {
+    const elements = document.body.getElementsByTagName('*'); // Setter alle elementer i skibidien til en variabel
+    for (let element of elements) {
+        //her må du bare koke matheo
+    }
+}
+
+translatePage(); //call
