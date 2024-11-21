d3cc727f38dbd12a40637a9caf9b13249fdc651c
henrik2123 <henrik.pedersen@intility.no>
2024-11-21 16:05:33 +0100

    Element whitelist, translate uten vowel

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,20 @@
 function replaceWithUzz(word) {
     const vowels = 'aeiouAEIOU';
+    let hasVowel = false;
     for (let i = word.length - 1; i >= 0; i--) {
         if (vowels.includes(word[i])) {
+            hasVowel = true;
             return word.substring(0, i) + 'uzz';
         }
+    }
+    if (!hasVowel) { 
+        return word + 'uzz';
     }
     return word;
 }
 
 function translatePage() {
-    const elements = document.body.getElementsByTagName('*');
+    const elements = document.body.getElementsByTagName('p'); //Må nesten bare "whiteliste" hvilke elementer som skal bli oversatt, hvis ikke blir allt knulla
     for (let element of elements) {
         for (let node of element.childNodes) {
             if (node.nodeType === 3) { // Text node
