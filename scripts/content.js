6e4da0582498f95dc2d250b7df3395b4ff1f18db
Matheo Kant Pangopoulos / Intility AS <matheo.kant.pangopoulos@intility.no>
2024-11-22 08:31:24 +0100

    It works

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,8 @@
 function replaceWithUzz(word) {
-    const vowels = 'aeiouAEIOU';
+    const vowels = 'aeiouyæøå';
     let hasVowel = false;
     for (let i = word.length - 1; i >= 0; i--) {
-        if (vowels.includes(word[i])) {
+        if (vowels.includes(word[i].toLowerCase())) {
             hasVowel = true;
             return word.substring(0, i) + 'uzz';
         }
@@ -13,17 +13,46 @@ function replaceWithUzz(word) {
     return word;
 }
 
-function translatePage() {
-    const elements = document.body.getElementsByTagName('p'); //Må nesten bare "whiteliste" hvilke elementer som skal bli oversatt, hvis ikke blir allt knulla
-    for (let element of elements) {
-        for (let node of element.childNodes) {
-            if (node.nodeType === 3) { // Text node
-                const words = node.nodeValue.split(' ');
-                const translatedWords = words.map(replaceWithUzz);
-                node.nodeValue = translatedWords.join(' ');
-            }
+function translateTextNodes(element) {
+    for (let node of element.childNodes) {
+        if (node.nodeType === 3 && node.nodeValue.trim() !== '') { // Text node and not empty
+            const words = node.nodeValue.split(' ');
+            const translatedWords = words.map(replaceWithUzz);
+            node.nodeValue = translatedWords.join(' ');
+        } else if (node.nodeType === 1) { // Element node
+            translateTextNodes(node); // Recursively translate text nodes in child elements
         }
     }
 }
 
+function translatePage() {
+    const elements = document.body.getElementsByTagName('*');
+    for (let element of elements) {
+        translateTextNodes(element);
+    }
+}
+
+// Initial translation
 translatePage();
+
+// Use MutationObserver to watch for dynamically added content
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+            mutation.addedNodes.forEach((node) => {
+                if (node.nodeType === 1) { // Element node
+                    // Delay processing to ensure content is fully loaded
+                    setTimeout(() => {
+                        translateTextNodes(node);
+                        const descendants = node.getElementsByTagName('*');
+                        for (let descendant of descendants) {
+                            translateTextNodes(descendant);
+                        }
+                    }, 500);
+                }
+            });
+        }
+    });
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
